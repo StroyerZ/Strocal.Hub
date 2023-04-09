bff5871f8cac425980e9ca4ff163969cd70e8155
StroyerZ <113297228+StroyerZ@users.noreply.github.com>
2023-04-09 13:48:39 +0200

    Update fly.lua

--- Scripts/fly.lua
+++ Scripts/fly.lua
@@ -1,81 +1,49 @@
- repeat wait()
-   until game.Players.LocalPlayer and game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:findFirstChild("Torso") and game.Players.LocalPlayer.Character:findFirstChild("Humanoid")
-local mouse = game.Players.LocalPlayer:GetMouse()
-repeat wait() until mouse
-local plr = game.Players.LocalPlayer
-local torso = plr.Character.Torso
-local flying = true
-local deb = true
-local ctrl = {f = 0, b = 0, l = 0, r = 0}
-local lastctrl = {f = 0, b = 0, l = 0, r = 0}
-local maxspeed = 50
-local speed = 0
+ --Variable
+local player = game.Players.LocalPlayer
 
-function Fly()
-local bg = Instance.new("BodyGyro", torso)
-bg.P = 9e4
-bg.maxTorque = Vector3.new(9e9, 9e9, 9e9)
-bg.cframe = torso.CFrame
-local bv = Instance.new("BodyVelocity", torso)
-bv.velocity = Vector3.new(0,0.1,0)
-bv.maxForce = Vector3.new(9e9, 9e9, 9e9)
-repeat wait()
-plr.Character.Humanoid.PlatformStand = true
-if ctrl.l + ctrl.r ~= 0 or ctrl.f + ctrl.b ~= 0 then
-speed = speed+.5+(speed/maxspeed)
-if speed > maxspeed then
-speed = maxspeed
-end
-elseif not (ctrl.l + ctrl.r ~= 0 or ctrl.f + ctrl.b ~= 0) and speed ~= 0 then
-speed = speed-1
-if speed < 0 then
-speed = 0
+--Globals // basically sets how fast you go.
+getgenv().Speed = 50
+getgenv().Distance = 5
+
+--NoClip
+local Noclipping = nil
+Clip = false
+wait(0.1)
+local function NoclipLoop()
+if Clip == false and player.Character ~= nil then
+for _, child in pairs(player.Character:GetDescendants()) do
+if child:IsA("BasePart") and child.CanCollide == true and child.Name ~= floatName then
+child.CanCollide = false
 end
 end
-if (ctrl.l + ctrl.r) ~= 0 or (ctrl.f + ctrl.b) ~= 0 then
-bv.velocity = ((game.Workspace.CurrentCamera.CoordinateFrame.lookVector * (ctrl.f+ctrl.b)) + ((game.Workspace.CurrentCamera.CoordinateFrame * CFrame.new(ctrl.l+ctrl.r,(ctrl.f+ctrl.b)*.2,0).p) - game.Workspace.CurrentCamera.CoordinateFrame.p))*speed
-lastctrl = {f = ctrl.f, b = ctrl.b, l = ctrl.l, r = ctrl.r}
-elseif (ctrl.l + ctrl.r) == 0 and (ctrl.f + ctrl.b) == 0 and speed ~= 0 then
-bv.velocity = ((game.Workspace.CurrentCamera.CoordinateFrame.lookVector * (lastctrl.f+lastctrl.b)) + ((game.Workspace.CurrentCamera.CoordinateFrame * CFrame.new(lastctrl.l+lastctrl.r,(lastctrl.f+lastctrl.b)*.2,0).p) - game.Workspace.CurrentCamera.CoordinateFrame.p))*speed
-else
-bv.velocity = Vector3.new(0,0.1,0)
 end
-bg.cframe = game.Workspace.CurrentCamera.CoordinateFrame * CFrame.Angles(-math.rad((ctrl.f+ctrl.b)*50*speed/maxspeed),0,0)
-until not flying
-ctrl = {f = 0, b = 0, l = 0, r = 0}
-lastctrl = {f = 0, b = 0, l = 0, r = 0}
-speed = 0
-bg:Destroy()
-bv:Destroy()
-plr.Character.Humanoid.PlatformStand = false
 end
-mouse.KeyDown:connect(function(key)
-if key:lower() == "e" then
-if flying then flying = false
-else
-flying = true
-Fly()
+
+--TP
+function tp(target)
+   --noclip
+   Noclipping = game:GetService('RunService').Stepped:connect(NoclipLoop)
+   player.Character.HumanoidRootPart.CFrame = player.Character.HumanoidRootPart.CFrame
+   game:GetService("TweenService"):Create(player.Character.HumanoidRootPart, TweenInfo.new(player:DistanceFromCharacter(target.Position)/getgenv().Speed, Enum.EasingStyle.Linear), {CFrame = target.CFrame + Vector3.new(0, Distance, 0)}):Play()
+   wait(player:DistanceFromCharacter(target.Position)/getgenv().Speed)
 end
-elseif key:lower() == "w" then
-ctrl.f = 1
-elseif key:lower() == "s" then
-ctrl.b = -1
-elseif key:lower() == "a" then
-ctrl.l = -1
-elseif key:lower() == "d" then
-ctrl.r = 1
-end
-end)
-mouse.KeyUp:connect(function(key)
-if key:lower() == "w" then
-ctrl.f = 0
-elseif key:lower() == "s" then
-ctrl.b = 0
-elseif key:lower() == "a" then
-ctrl.l = 0
-elseif key:lower() == "d" then
-ctrl.r = 0
-end
-end)
-Fly()
- 
+
+-- Script
+--[[ Get the path to the part you want to tween (teleport) to and put it under here ]]--
+
+local path = game.Worskapce.part -- obviously change part with what you need
+
+tp(path) -- This will tween bring you to said part
+
+--[[ Makes your character lock itself in the air. There's many ways of doing this but this is the simplest. It can make some things unusable depending on the game, though. ]]--
+player.Character.HumanoidRootPart.Anchored = true
+
+-- do what you want after here V
+
+
+wait(5) -- after this wait, it will turn the noclip off and back your body go back to falling.
+
+-- Clip // turns off the noclip --
+Noclipping:Disconnect()
+Clip = true
+player.Character.HumanoidRootPart.Anchored = false
